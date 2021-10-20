49b2f0dc4cb9d171dd7f82a7f8fae451b18ade8f
Phairoj Thamrongthanyawong <phaoroj_t@yahoo.com>
2021-10-21 05:02:12 +0700

    add native prototype

---
+++ React/NativeProto/nativeproto.js
@@ -0,0 +1,5 @@
+// ให้เพิ่ม Method defer เข้าไปใน prototypes ของทุกฟังก์ชัน โดย Method นี้จะทำหน้าที่ alert ข้อความออกมาหลังจากผ่านไป ms
+
+Function.prototype.defer = function (ms) {
+	setTimeout(this, ms);
+};
